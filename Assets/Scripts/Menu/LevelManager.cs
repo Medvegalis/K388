71b7efbcfb40f09b567b857f6a414caa53a0636b
Medvegalis <mmedeksa8@gmail.com>
2024-02-29 22:10:43 +0200

    Added VR Main menu
    -Added Lobby scene with main menu and interactable objects
    -Added VR player prefabs to both scenes

---
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    private int lobbyIndex = 0;
+    private int level1Index = 1;
+    private int level2Index = 2;
+    void Start()
+    {
+        
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadScene(lobbyIndex);
+    }
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene(level1Index);
+    }
+    public void StartLevel2()
+    {
+        SceneManager.LoadScene(level2Index);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
