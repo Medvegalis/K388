5c5ae68d7e082a1e02eb4023babe753f363bd181
Efka147 <146087717+Efka147@users.noreply.github.com>
2024-03-21 00:00:50 +0200

    Working slide preview
    Works only with pdf
    
    PDF Reader Guide Manual
    Upon receiving several requests for guide to setup pdf reader in Unity, here is the checklist to ensure that it works smooth for you.
    
    Make sure that the libs folder contains all the necessary libraries.
    
    pdfium.dll
    PdfToImage.dll
    PdfiumViewer.dll
    System.Drawing.Design.dll
    PdfiumViewer.resources.dll
    System.Drawing.dll
    Playser Settings:
    
    ScriptingRuntimeVersion -> .NET4.x equivalent
    ScriptingImplementation -> Mono
    ApiCompatibilityLevel -> .NET 4.6
    Make sure that the file path is set correctly and finally in the build folder copy and paste the following dll's:
    
    pdfium.dll
    PdfiumViewer.dll
    PdfiumViewer.resources.dll
    right next to your build executable and you should be good to go.
    
    credits: Alexey (https://github.com/vangogih)

---
+++ Assets/Scripts/PdfReader.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Drawing;
+using UnityEngine.UI;
+using System.IO;
+using Shubham.PDF;
+
+//using Image = System.Drawing;
+//C:\Users\shubh\Downloads\Docs\HKUST_EDX_Certificate.pdf
+
+public class PdfReader : MonoBehaviour {
+	
+	public int totalPageCnt = 0;
+	public UnityEngine.UI.RawImage pdfSceneImg;
+
+	private int currPageIndex = 0;
+	private PdfToImage pti;
+    //private List<Texture2D> pdfPages = new List<Texture2D> ();
+
+    //C:\Users\shubh\Documents\Unity_Projects\OpenCVTest\Assets\StreamingAssets\cert.pdf
+    public void Start()
+    {
+		InitialisePdf(Directory.GetCurrentDirectory()+@"\Assets\Resources\slides.pdf");
+    }
+
+    public void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			ShowNextPage();
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+			ShowPrevPage();
+    }
+
+
+    public void InitialisePdf(string path)
+	{
+		//pdfPages.Clear ();
+		totalPageCnt = 0;
+		//Debug.Log(path.text);
+		pti = new PdfToImage ();
+		PdfToImage.logs += PdfLog;
+
+				Debug.Log (pti.Test());
+		Debug.Log(path);
+		if (pti.ReadPdf (path, ref totalPageCnt))
+			 Debug.Log("Page: (" + (currPageIndex + 1).ToString () + "/" + totalPageCnt.ToString () + ")");
+		else
+			Debug.Log("unable to read");
+		
+		pdfSceneImg.texture = pti.GetDrawImageFromPdf(0);
+		//pdfSceneImg.texture = pdfPages [0];
+	}
+	/*
+	public Texture2D ConvertImageToTexture(System.Drawing.Image image)
+	{
+		Texture2D tex = new Texture2D (image.Width, image.Height);
+		MemoryStream ms = new MemoryStream ();
+		image.Save (ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+		
+		ms.Seek (0, SeekOrigin.Begin);
+		tex.LoadImage (ms.ToArray ());
+		
+		ms.Close ();
+		ms = null;
+
+		return tex;
+	}*/
+
+	public void ShowNextPage()
+	{
+		if (currPageIndex >= totalPageCnt-1)
+			return;
+
+		currPageIndex++;
+		
+		pdfSceneImg.texture = pti.GetDrawImageFromPdf(currPageIndex);
+		Debug.Log("Page: (" + (currPageIndex + 1).ToString() + "/" + totalPageCnt.ToString() + ")");
+	}
+
+	public void ShowPrevPage()
+	{
+		if (currPageIndex < 1)
+			return;
+
+		currPageIndex--;
+
+		pdfSceneImg.texture = pti.GetDrawImageFromPdf(currPageIndex);
+		Debug.Log("Page: (" + (currPageIndex + 1).ToString() + "/" + totalPageCnt.ToString() + ")");
+	}
+
+	public void PdfLog(string msg)
+	{
+		Debug.Log ("[PDF]: " + msg);
+	}
+}
